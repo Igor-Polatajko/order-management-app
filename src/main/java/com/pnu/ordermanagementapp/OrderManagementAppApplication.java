5f1de8c5ddbac32c8c1fe730ef29a06d056b2153
Igor-Polatajko <igor.bogdanovich39@gmail.com>
2020-04-12 21:05:02 +0300

    Initial commit

---
+++ src/main/java/com/pnu/ordermanagementapp/OrderManagementAppApplication.java
@@ -0,0 +1,13 @@
+package com.pnu.ordermanagementapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderManagementAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderManagementAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pnu/ordermanagementapp/OrderManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pnu.ordermanagementapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderManagementAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
